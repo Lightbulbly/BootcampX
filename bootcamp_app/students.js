e7930286dc81399ed36f7b6e7d06d9d4f6fd9b56
Noya Onggud <yanuo44@gmail.com>
2022-01-06 01:13:12 -0500

    Update students.js & teachers.js to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,32 +9,18 @@ const client = new Client({
 
 client.connect();
 
-let myArgs = process.argv.slice(2);
-const queryCohortName = myArgs[0];
-const  maximumNumberOfResults = myArgs[1].toUpperCase();
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
 
-// client.query(`
-// SELECT students.id as id, students.name as name, cohorts.name as cohort
-// FROM students
-// JOIN cohorts ON students.cohort_id=cohorts.id
-// WHERE cohorts.name LIKE '%${queryCohortName}%'
-// LIMIT ${maximumNumberOfResults};
-// `)
-//   .then(res => {
-//     res.rows.forEach(user=> {
-//       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-//     }
-//     );
-//   })
-//   .catch(err => console.error('query error', err.stack));
-
-client.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+client.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,30 +9,18 @@ const client = new Client({
 
 client.connect();
 
-// client.query(`
-// SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
-// FROM teachers
-// JOIN assistance_requests ON teacher_id = teachers.id
-// JOIN students ON student_id = students.id
-// JOIN cohorts ON cohort_id = cohorts.id
-// WHERE cohorts.name LIKE '%${process.argv[2]}%'
-// ORDER BY teacher;
-// `)
-//   .then(res => {
-//     res.rows.forEach(user => {
-//       console.log(`${process.argv[2]}: ${user.teacher}`);
-//     });
-//   }).catch(err => console.error('query error', err.stack));
-
-client.query(`
+const values = [process.argv[2] || 'JUL02'];
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+
+client.query(queryString, values)
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
